956850f69ae63908663acb90180777a83d46912b
Rajesh Aradhye <raradhye@gmail.com>
2025-07-22 22:41:39 -0700

    forget to save variables.tf file

--- main/variables.tf
+++ main/variables.tf
@@ -11,21 +11,11 @@ variable "naming_prefix" {
   type    = string
   default = "adolabs"
 }
-
-variable "asp_tier" {
-  type        = string
-  description = "Tier for App Service Plan (Standard, PremiumV2)"
-  default     = "Free"
+variable "os_type" {
+  type    = string
+  default = "Linux"
 }
-
-variable "asp_size" {
-  type        = string
-  description = "Size for App Service Plan (S2, P1v2)"
-  default     = "S1"
-}
-
-variable "capacity" {
-  type        = string
-  description = "Number of instances for App Service Plan"
-  default     = "1"
+variable "sku_name" {
+  type    = string
+  default = "S1"
 }
