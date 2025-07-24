a95e22773bd5e2f4eb13148ce8e08d7bc3c2f1ee
Rajesh Aradhye <raradhye@gmail.com>
2025-07-24 15:56:13 -0700

    fix node_version in the application stack in main.tf

--- main/main.tf
+++ main/main.tf
@@ -43,7 +43,7 @@ resource "azurerm_windows_web_app" "app_service" {
   site_config {
     application_stack {
       current_stack = "node"
-      node_version  = "18-lts" # You can change this based on your needs
+      node_version  = "~18" # You can change this based on your needs
     }
   }
   # checkov:skip=CKV_AZURE_222: Public access is acceptable in this demo
